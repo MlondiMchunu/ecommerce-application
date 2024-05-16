0ea9db300a409427c4b9b2b312dad3bd926a879e
Stopa <sikhuliso@gmail.com>
2024-05-16 22:19:08 +0200

    app changes

--- app.js
+++ app.js
@@ -6,8 +6,8 @@ app.get("/",(req,res)=>{
     res.send("Hello from Node");
 });
 
-const port = process.env.PORT || 8000
+const port = process.env.PORT || 8000;
 
 app.listen(port,()=>{
     console.log(`Server is running on port ${port}`)
-})
+});
